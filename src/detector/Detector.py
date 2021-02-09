4430a991dda86ac293b7ddc9077ca72b2538b895
WOOSEUNGHOON <toad58@nate.com>
2021-02-10 01:09:35 +0900

    eliminate dependency

--- src/detector/Detector.py
+++ src/detector/Detector.py
@@ -7,8 +7,8 @@ Modified: 	December 16, 2020.
 
 import os
 import sys
-sys.path.insert(0, "../osscollector")
-import OSS_Collector
+#sys.path.insert(0, "../osscollector")
+#import OSS_Collector
 import re
 import shutil
 import json
@@ -33,6 +33,103 @@ for eachRepo in shouldMake:
 		os.mkdir(eachRepo)
 
 
+# Generate TLSH
+def computeTlsh(string):
+	string 	= str.encode(string)
+	hs 		= tlsh.forcehash(string)
+	return hs
+
+
+def removeComment(string):
+	# Code for removing C/C++ style comments. (Imported from VUDDY and ReDeBug.)
+	# ref: https://github.com/squizz617/vuddy
+	c_regex = re.compile(
+		r'(?P<comment>//.*?$|[{}]+)|(?P<multilinecomment>/\*.*?\*/)|(?P<noncomment>\'(\\.|[^\\\'])*\'|"(\\.|[^\\"])*"|.[^/\'"]*)',
+		re.DOTALL | re.MULTILINE)
+	return ''.join([c.group('noncomment') for c in c_regex.finditer(string) if c.group('noncomment')])
+
+def normalize(string):
+	# Code for normalizing the input string.
+	# LF and TAB literals, curly braces, and spaces are removed,
+	# and all characters are lowercased.
+	# ref: https://github.com/squizz617/vuddy
+	return ''.join(string.replace('\n', '').replace('\r', '').replace('\t', '').replace('{', '').replace('}', '').split(' ')).lower()
+
+def hashing(repoPath):
+	# This function is for hashing C/C++ functions
+	# Only consider ".c", ".cc", and ".cpp" files
+	possible = (".c", ".cc", ".cpp")
+	
+	fileCnt  = 0
+	funcCnt  = 0
+	lineCnt  = 0
+
+	resDict  = {}
+
+	for path, dir, files in os.walk(repoPath):
+		for file in files:
+			filePath = os.path.join(path, file)
+
+			if file.endswith(possible):
+				try:
+					# Execute Ctgas command
+					functionList 	= subprocess.check_output(ctagsPath + ' -f - --kinds-C=* --fields=neKSt "' + filePath + '"', stderr=subprocess.STDOUT, shell=True).decode()
+
+					f = open(filePath, 'r', encoding = "UTF-8")
+
+					# For parsing functions
+					lines 		= f.readlines()
+					allFuncs 	= str(functionList).split('\n')
+					func   		= re.compile(r'(function)')
+					number 		= re.compile(r'(\d+)')
+					funcSearch	= re.compile(r'{([\S\s]*)}')
+					tmpString	= ""
+					funcBody	= ""
+
+					fileCnt 	+= 1
+
+					for i in allFuncs:
+						elemList	= re.sub(r'[\t\s ]{2,}', '', i)
+						elemList 	= elemList.split('\t')
+						funcBody 	= ""
+
+						if i != '' and len(elemList) >= 8 and func.fullmatch(elemList[3]):
+							funcStartLine 	 = int(number.search(elemList[4]).group(0))
+							funcEndLine 	 = int(number.search(elemList[7]).group(0))
+
+							tmpString	= ""
+							tmpString	= tmpString.join(lines[funcStartLine - 1 : funcEndLine])
+
+							if funcSearch.search(tmpString):
+								funcBody = funcBody + funcSearch.search(tmpString).group(1)
+							else:
+								funcBody = " "
+
+							funcBody = removeComment(funcBody)
+							funcBody = normalize(funcBody)
+							funcHash = computeTlsh(funcBody)
+
+							if len(funcHash) == 72 and funcHash.startswith("T1"):
+								funcHash = funcHash[2:]
+							elif funcHash == "TNULL" or funcHash == "" or funcHash == "NULL":
+								continue
+
+							storedPath = filePath.replace(repoPath, "")
+							if funcHash not in resDict:
+								resDict[funcHash] = []
+							resDict[funcHash].append(storedPath)
+
+							lineCnt += len(lines)
+							funcCnt += 1
+
+				except subprocess.CalledProcessError as e:
+					print("Parser Error:", e)
+					continue
+				except Exception as e:
+					print ("Subprocess failed", e)
+					continue
+
+	return resDict, fileCnt, funcCnt, lineCnt 
 
 def getAveFuncs():
 	aveFuncs = {}
@@ -179,7 +276,9 @@ def detector(inputDict, inputRepo):
 
 
 def main(inputPath, inputRepo):
-	resDict, fileCnt, funcCnt, lineCnt = OSS_Collector.hashing(inputPath)
+	resDict, fileCnt, funcCnt, lineCnt = hashing(inputPath)
+
+
 	detector(resDict, inputRepo)
 
 
